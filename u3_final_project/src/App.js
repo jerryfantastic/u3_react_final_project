bb6b5d11b0f6c77f4ec5b5226dc611e1b73fadab
Jerry Fan <jerry@jerryfantastic.com>
2022-07-18 10:22:55 -0700

    copied kanye quote files

--- u3_final_project/src/App.js
+++ u3_final_project/src/App.js
@@ -1,24 +1,36 @@
-import { useState } from 'react'
-import './styles/App.css'
+import { useState } from "react";
+import "./styles/App.css";
+import KanyeQuote from "./components/KanyeQuote";
 
 const App = () => {
-  const [displayQuote, setDisplayQuote] = useState(false)
+  const [displayQuote, setDisplayQuote] = useState(false);
 
   const toggleQuotes = () => {
-
-  }
+    if (displayQuote === false) {
+      setDisplayQuote(true);
+    } else {
+      setDisplayQuote(false);
+    }
+  };
 
   return (
     <div className="App">
       <header className="kanye-header"></header>
       <main>
         <div className="quote-container">
-          <h2>Need some inspiration? See what Kanye thinks.</h2>
+          {/* conditional rendering; depending on if displayQuote true or false, something different is displayed */}
+          {displayQuote === false ? (
+            <h2>Need some inspiration? See what Kanye thinks.</h2>
+          ) : (
+            <KanyeQuote displayQuote={displayQuote} />
+          )}
         </div>
-        <button>New Quote</button>
+        <button onClick={toggleQuotes}>
+          {displayQuote === false ? "New Quote" : "Clear Quote"}
+        </button>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ u3_final_project/src/components/KanyeQuote.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+
+
+const KanyeQuote = ({displayQuote}) => {
+
+    const [quote, setQuote] = useState('')
+
+    const getKanyeQuote = async () => {
+    
+    
+       
+    
+        try {
+            const res = await axios.get(`https://api.kanye.rest`)
+            console.log(res)
+
+            setQuote(res.data.quote)
+    
+    
+        } catch(err) {
+          console.log(err)
+        }
+    
+      }
+    
+    
+    useEffect(() => {
+        getKanyeQuote()
+      }, [displayQuote])
+
+
+
+    return (
+        <h2 style={{fontStyle: 'italic'}}>{quote !== '' && quote}</h2>
+
+    )
+  }
+  
+  export default KanyeQuote
