30c43653b3d099cfdfeee983c53d557024d931f9
Jerry Fan <jerry@jerryfantastic.com>
2022-07-19 00:16:03 -0700

    added hex code, converted from rgb

--- u3_final_project/src/components/ColorSwatch.jsx
+++ u3_final_project/src/components/ColorSwatch.jsx
@@ -1,11 +1,23 @@
 const ColorSwatch = (props) => {
 
+    const componentToHex = (c) => {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
+    }
+
+    const rgbToHex = (r, g, b) => {
+        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    }
+
+
+
+
     return (
         <div className="color_swatch" style={{
             backgroundColor: `rgb(${props.colorSwatch[0]}, ${props.colorSwatch[1]},${props.colorSwatch[2]})`
         }}>
 
-
+            <span className="color_swatch_hex">{rgbToHex(props.colorSwatch[0], props.colorSwatch[1], props.colorSwatch[2])}</span>
         </div>
     )
 }
